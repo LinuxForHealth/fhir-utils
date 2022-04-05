b93266bd82affb198b5dddbcab3cde494c375f63
Henry Feldman <henry.feldman@ibm.com>
2022-04-05 12:05:52 -0400

    update to package structure to accomodate strange folder name {Library Name} which was auto-created.

--- src/tests/test_lib.py
+++ src/tests/test_lib.py
@@ -8,18 +8,19 @@ import json
 from fhir.resources.humanname import HumanName
 from fhir.resources.patient import Patient
 from typing import List
-import /src/linuxforhealth/{library name}/fhir_text_utils
+from src.linuxforhealth.fhir_utils.fhir_text_utils import humannameasstring
 
 
 # Arrange json
-@pytest.fixture
 def load_test_patient():
     patient_file = open('../linuxforhealth/{library name}/patient.json')
     patient_json_dict = json.load(patient_file)
     test_patient: Patient = Patient.parse_obj(patient_json_dict)
-    assert(fhir_text_utils.humannameasstring(test_patient.name) == "Hart  III, Dr. Julia")
+    assert(test_patient)
+    assert(humannameasstring(test_patient.name) == "Hart  III, Dr. Julia")
+
 
 @pytest.fixture
 def test_humanname_to_str(load_test_patient):
     # Act
-    name_as_string = fhir_text_utils.humannameasstring(*load_test_patient.name, False)
+    name_as_string = humannameasstring(*load_test_patient.name, False)
