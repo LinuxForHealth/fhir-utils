a2b8ace2dbca90d614c49da0a235c48e768bebf1
Henry Feldman <henry.feldman@ibm.com>
2022-04-05 15:00:54 -0400

    update to pytest test fixtures

--- src/tests/test_lib.py
+++ src/tests/test_lib.py
@@ -11,16 +11,13 @@ from typing import List
 from src.linuxforhealth.fhir_utils.fhir_text_utils import humannameasstring
 
 
-# Arrange json
-def load_test_patient():
-    patient_file = open('../linuxforhealth/{library name}/patient.json')
-    patient_json_dict = json.load(patient_file)
-    test_patient: Patient = Patient.parse_obj(patient_json_dict)
-    assert(test_patient is not None)
-    assert(humannameasstring(test_patient.name) == "Hart  III, Dr. Julia")
-
-
 @pytest.fixture
-def test_humanname_to_str(load_test_patient):
-    # Act
-    name_as_string = humannameasstring(*load_test_patient.name, False)
+def test_patient()->Patient:
+    patient_file = open('patient.json')
+    patient_json_dict = json.load(patient_file)
+    return Patient.parse_obj(patient_json_dict)
+
+
+def run_test(test_patient):
+    assert test_patient is not None
+    assert humannameasstring(test_patient.name).strip() == "Hart  III, Dr. Julia"
